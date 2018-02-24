2320ea32e9bd2df2c583d5d050d94720c8d2306b
Reda Ramadan <king2sairos2013@gmail.com>
2018-02-24 19:50:35 +0200

    fix: Correct request parameters format on signup

--- Server/api/controllers/UserController.js
+++ Server/api/controllers/UserController.js
@@ -3,68 +3,71 @@ var mongoose = require('mongoose'),
   Validations = require('../utils/Validations'),
   User = mongoose.model('User');
 
-  module.exports.signUp = function(req, res, next) {
-    var valid =
-      req.body.fullName &&
-      Validations.isString(req.body.fullName) &&
-      req.body.emailAddress &&
-      Validations.isString(req.body.emailAddress) &&
-      req.body.password &&
-      Validations.isString(req.body.password);
-    if (!valid) {
-      return res.status(422).json({
-        err: null,
-        msg: 'Full name, email, and password are required fields.',
-        data: null
-      });
-    }
-    // Security Check
-    delete req.body.createdAt;
-    delete req.body.updatedAt;
-  
-    User.create(req.body, function(err, user) {
-      if (err) {
-        return next(err);
-      }
-      res.status(201).json({
-        err: null,
-        msg: 'User was created successfully.',
-        data: user
-      });
+module.exports.signUp = function (req, res, next) {
+  var valid =
+    req.body.fullName &&
+    Validations.isString(req.body.fullName) &&
+    req.body.emailAddress &&
+    Validations.isString(req.body.emailAddress) &&
+    req.body.password &&
+    Validations.isString(req.body.password);
+  if (!valid) {
+    return res.status(422).json({
+      err: null,
+      msg: 'Full name, email, and password are required fields.',
+      data: null
     });
-  };
+  }
+  // Security Check
+  delete req.body.createdAt;
+  delete req.body.updatedAt;
 
-  module.exports.login = function(req, res, next) {
-    if (!Validations.isString(req.params.emailAddress)) {
+  User.create(req.body, function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    res.status(201).json({
+      err: null,
+      msg: 'User was created successfully.',
+      data: user
+    });
+  });
+};
+
+module.exports.login = function (req, res, next) {
+  if (!Validations.isString(req.body.emailAddress)) {
+    return res.status(422).json({
+      err: null,
+      msg: 'Email parameter must be a valid email address.',
+      data: null
+    });
+  }
+
+  if (!Validations.isString(req.body.password)) {
       return res.status(422).json({
         err: null,
-        msg: 'Email parameter must be a valid email address.',
+        msg: 'Password parameter must be a valid password.',
         data: null
       });
     }
-
-    if (!Validations.isString(req.params.password)) {
-        return res.status(422).json({
-          err: null,
-          msg: 'Password parameter must be a valid password.',
-          data: null
-        });
-      }
-//to be added
-    User.findOne({'emailAddress': req.params.emailAddress, 'password': req.params.password})
-    .exec(function(err, user) {
+  //to be added
+  User.findOne({
+    emailAddress: req.body.emailAddress,
+    password: req.body.password
+  })
+    .exec(function (err, user) {
       if (err) {
         return next(err);
       }
       if (!user) {
         return res
           .status(404)
-          .json({ err: null, msg: 'User not found.', data: null });
+          .json({ err: null, msg: `User not found. ${req.body.emailAddress}: ${req.body.password}`, data: null });
       }
-      res.status(200).json({
+      return res.status(200).json({
         err: null,
         msg: 'User retrieved successfully.',
         data: user
       });
     });
-  };
+};
