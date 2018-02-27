a16c6685059e1e3cedd532457a9ca0a5f23094f4
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-02-27 18:24:24 +0200

    fix: minor fix to solve cross-platform issues

--- Client/src/app/user.service.ts
+++ Client/src/app/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import  {Observable } from 'rxjs/observable';
-import { of } from 'rxjs/observable/of';
+import  {Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
