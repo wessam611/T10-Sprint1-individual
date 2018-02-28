49603bd9ee8bd69bbd2fe4f5f6525dca80828df0
Nesrin Abdulaziz <nesrin.abdulaziz14@gmail.com>
2018-02-28 11:32:26 +0200

    Feat: add HTTP skeleton for orders
    missing: handling logged user

--- Client/src/app/orders/order.service.ts
+++ Client/src/app/orders/order.service.ts
@@ -5,10 +5,29 @@ import { orders } from './mock-orders';
 import {Order} from './Order';
 import {Product} from '../Product';
 import {products} from '../mock-products';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HeaderComponent } from '../@theme/components/header/header.component';
+import { catchError, map, tap } from 'rxjs/operators';
 @Injectable()
 export class OrderService {
-  getOrders(): Observable<Order[]> {
-  return of(orders);
+  private ordersUrl = '/user/1/orders'; //TODO
+getOrders (): Observable<Order[]> {
+  return this.http.get<Order[]>(this.ordersUrl)
+      .pipe(
+        catchError(this.handleError('getOrders', []))
+      );
 }
-  constructor() { }
+constructor(
+private http: HttpClient) { }
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+    // TODO: better job of transforming error for user consumption
+  //  this.log(`${operation} failed: ${error.message}`);
+
+    return of(result as T);
+  };
+}
 }
