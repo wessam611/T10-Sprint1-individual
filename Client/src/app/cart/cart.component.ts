1721b45fe7a92b06dceb68598e2a817c121e630e
Nesrin Abdulaziz <nesrin.abdulaziz14@gmail.com>
2018-03-01 12:52:24 +0200

    Feat: add checkout feature **requires testing

--- Client/src/app/cart/cart.component.ts
+++ Client/src/app/cart/cart.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Cart, Product } from '../difinitions';
 import { CartService } from '../cart.service';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {UserService} from '../user.service';
+import { Router } from '@angular/router';
+import {OrderService} from '../orders/order.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -10,17 +13,36 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   cart: Cart;
-
-  constructor(private cartService: CartService) { }
-
+  shippingAddress :string = null;
+  showModal: string = 'none';
+  constructor(private cartService: CartService, private orderService:OrderService,private userService:UserService,private router: Router) { }
   ngOnInit() {
     this.cart = this.cartService.getCartFromLocalStorage();
     // this.getCart(id);
   }
 
-  // Get Cart From The Service
   getCart(id: number): void {
     this.cartService.getCart(id).subscribe(cart => this.cart = cart);
   }
 
+  checkout():void{
+    if(this.userService.getUser()===null){
+      this.router.navigate(['auth/login']);
+    }
+    else{
+      this.showModal ='block';
+    }
+  }
+  postOrders(): void {
+    this.orderService.postOrders(this.shippingAddress).subscribe(function (res) {
+      if (res.msg === 'orders') {
+        alert(`Your order is on its way!`);
+      }
+    });
+    this.showModal = 'none';
+  }
+
+  closeModal():void{
+    this.showModal = 'none';
+  }
 }
